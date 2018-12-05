ad0947faacafb17f8b9ed7581012f9023a1aeb0c
andy black <andy_black@sil.org>
2018-12-05 15:51:21 -0700

    Use version 1.1.0 of libjavadev

--- src/org/sil/lingtree/Constants.java
+++ src/org/sil/lingtree/Constants.java
@@ -10,7 +10,7 @@ package org.sil.lingtree;
  *
  */
 public class Constants {
-	public static final String VERSION_NUMBER = "1.2.0";
+	public static final String VERSION_NUMBER = "1.2.1";
 	public static final int CURRENT_DATABASE_VERSION = 2;
 	public static final String MAC_OS_X = "Mac OS X";
 
