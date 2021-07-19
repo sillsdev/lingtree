c80b323927aae1712efc0b33856e7ca12478aab3
andy black <blackhandrew@gmail.com>
2021-07-19 13:32:04 -0700

    Update version number Add JAXB license

--- src/org/sil/lingtree/Constants.java
+++ src/org/sil/lingtree/Constants.java
@@ -10,7 +10,7 @@ package org.sil.lingtree;
  *
  */
 public class Constants {
-	public static final String VERSION_NUMBER = "1.2.6";
+	public static final String VERSION_NUMBER = "1.2.7";
 	public static final int CURRENT_DATABASE_VERSION = 2;
 
 	public static final String SUBSCRIPT = "/s";
