0384ba266c3d43aaa6b99d6c6212e0610082d9ae
Andy Black <blackhandrew@gmail.com>
2024-10-17 15:36:05 -0700

    Debug CreateJar.bat pass 2

--- installer/windows/CreateJar.bat
+++ installer/windows/CreateJar.bat
@@ -1,8 +1,13 @@
-REM @echo off
-cd ../../binc
+@echo off
+echo before cd to bin
+cd ../../bin
 REM jar cmf META-INF\MANIFEST.MF pcpatreditor.jar .
 REM echo java_path=%1
+echo before jar
 jar cf lingtree.jar .
+echo copy
 copy lingtree.jar ../installer/windows/input > nul
+echo del
 del lingtree.jar > nul
+echo cd
 cd ../installer/windows
