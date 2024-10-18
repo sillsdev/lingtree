6889e3e0ca03be372a390b69fe4433394ec542de
Andy Black <blackhandrew@gmail.com>
2024-10-18 15:10:26 -0700

    Debug CreateJar.bat pass 20

--- installer/windows/CreateJar.bat
+++ installer/windows/CreateJar.bat
@@ -2,9 +2,10 @@
 pwd
 echo before cd to bin
 cd ..\..\bin
-echo before go up two levels; is bin there?
-cd ..\..
 dir
+REM echo before go up two levels; is bin there?
+REM cd ..\..
+REM dir
 REM jar cmf META-INF\MANIFEST.MF pcpatreditor.jar .
 REM echo java_path=%1
 echo before jar
@@ -13,5 +14,7 @@ echo copy
 copy lingtree.jar ../installer/windows/input > nul
 echo del
 del lingtree.jar > nul
-echo cd
+echo cd to installer windows
 cd ..\installer\windows
+dir
+
