396ca11efa9260e2cc5edde376e5fa8d8d2e6aa8
Andy Black <blackhandrew@gmail.com>
2024-10-23 11:12:10 -0700

    Try to add LTBatch.bat to app directory 4

--- installer/windows/MoveResources.bat
+++ installer/windows/MoveResources.bat
@@ -4,4 +4,4 @@ xcopy output\LingTree\app\resources output\LingTree\resources /E/s/i > nul
 rmdir output\LingTree\app\resources /S /q > nul
 xcopy output\LingTree\app\doc output\LingTree\doc /E/s/i > nul
 rmdir output\LingTree\app\doc /S /q > nul
-copy ..\..\build\package\windows\LTBatch.bat output\LingTree /Y  > nul
+copy ..\..\build\package\windows\LTBatch.bat output\LingTree\app /Y  > nul
