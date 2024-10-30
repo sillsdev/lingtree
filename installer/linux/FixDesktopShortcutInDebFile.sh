015cbffb9f7df8d57a172c7615c3b48ff4cd75dc
Andy Black <blackhandrew@gmail.com>
2024-10-30 09:25:34 -0700

    Build Linux Installer on GitHub 13

--- installer/linux/FixDesktopShortcutInDebFile.sh
+++ installer/linux/FixDesktopShortcutInDebFile.sh
@@ -5,6 +5,8 @@ if [ -d debtemp ]; then
 fi
 echo "	fixing desktop shortcut in deb file"
 mkdir debtemp
+pwd
+ls -R
 DEB_FILE=`ls lingtree_1.0-1_amd64.deb`
 dpkg-deb -R $DEB_FILE debtemp
 sed '/^Exec=*/a Path=/opt/sil/lingtree/bin/' debtemp/opt/sil/lingtree/lib/lingtree-LingTree.desktop >debtemp/opt/sil/lingtree/lib/lingtree-LingTree2.desktop
