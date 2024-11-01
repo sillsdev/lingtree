1d8da2856aa0541a15cce00fa4ba6bfa2123e699
Andy Black <blackhandrew@gmail.com>
2024-11-01 14:57:21 -0700

    Build macOS Installer on GitHub 17

--- installer/macos/MoveResources.sh
+++ installer/macos/MoveResources.sh
@@ -3,11 +3,13 @@
 # mkdir -p output//PcPatrEditor/lib/app
 # cp -r input/resources output/ > /dev/null
 # we run from bin and the executable looks here for the resources
-echo 'input has'
+echo 'input/resources has'
 ls -l -R input/resources
+echo 'before input chmod'
+chmod +x input/resources/Keyboards/macOS/xkbswitch
 echo 'before copy'
 ls -l -R output/LingTree/
-cp -r input/resources output/LingTree > /dev/null
+cp -r input/resources output/LingTree/resources > /dev/null
 echo 'after copy'
 ls -l -R output/LingTree/
 chmod +x output/LingTree/resources/Keyboards/macOS/xkbswitch
