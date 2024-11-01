91663fa835409c1b68394d493391ec678a371aa6
Andy Black <blackhandrew@gmail.com>
2024-11-01 14:14:42 -0700

    Build macOS Installer on GitHub 12

--- installer/macos/MoveResources.sh
+++ installer/macos/MoveResources.sh
@@ -4,9 +4,9 @@
 # cp -r input/resources output/ > /dev/null
 # we run from bin and the executable looks here for the resources
 cp -r input/resources/* output/LingTree > /dev/null
-ls -l -R output/LingTree/r*
+ls -l -R output/LingTree/
 chmod +x output/LingTree/resources/Keyboards/macOS/xkbswitch
-ls -l -R output/LingTree/r*
+ls -l -R output/LingTree/
 #rm -r input/PcPatrEditor/lib/app/resources > /dev/null
 cp -r input/doc output/LingTree > /dev/null
 #rm -r input/PcPatrEditor/lib/app/doc > /dev/null
