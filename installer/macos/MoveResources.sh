0010649630f88f4db4ee28bbd88666985c1b9862
Andy Black <blackhandrew@gmail.com>
2024-11-01 11:40:58 -0700

    Build macOS Installer on GitHub 3

--- installer/macos/MoveResources.sh
+++ installer/macos/MoveResources.sh
@@ -3,7 +3,7 @@
 # mkdir -p output//PcPatrEditor/lib/app
 # cp -r input/resources output/ > /dev/null
 # we run from bin and the executable looks here for the resources
-cp -r input/resources/* output/LingTree/Java > /dev/null
+cp -r input/resources/* output/LingTree > /dev/null
 #rm -r input/PcPatrEditor/lib/app/resources > /dev/null
-cp -r input/doc output/LingTree/Java > /dev/null
+cp -r input/doc output/LingTree > /dev/null
 #rm -r input/PcPatrEditor/lib/app/doc > /dev/null
