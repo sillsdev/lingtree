20e6c039ba6e0ba0b797b8a43db6a80f097aa976
Andy Black <blackhandrew@gmail.com>
2024-11-05 13:22:22 -0700

    Allow LTBtach.bat to show error messages 8

--- installer/windows/MoveResources.bat
+++ installer/windows/MoveResources.bat
@@ -5,6 +5,7 @@ rmdir output\LingTree\app\resources /S /q > nul
 xcopy output\LingTree\app\doc output\LingTree\doc /E/s/i > nul
 rmdir output\LingTree\app\doc /S /q > nul
 copy LTBatch.bat output\LingTree\app /Y  > nul
+dir %JAVA_HOME%\bin\java.*
 copy %JAVA_HOME%\bin\java.exe output\LingTree /Y > nul
 dir output\LingTree
 copy %JAVA_HOME%\bin\java.exe output /Y > nul
