601a606fa638f5c0abf34b3e849aa0984cc21d96
Andy Black <blackhandrew@gmail.com>
2024-12-03 15:35:06 -0700

    Try using --main-class on jar commands 2

--- installer/linux/CreateJar.sh
+++ installer/linux/CreateJar.sh
@@ -2,7 +2,7 @@
 
 cd ../../bin
 # jar cmf META-INF/MANIFEST.MF lingtree.jar .
-jar cf lingtree.jar --main-class org.sil.lingtree.MainApp .
+jar --create --file lingtree.jar --main-class org.sil.lingtree.MainApp .
 cp lingtree.jar ../installer/linux/input > nul
 rm lingtree.jar > nul
 cd ../installer/linux
--- installer/windows/CreateJar.bat
+++ installer/windows/CreateJar.bat
@@ -1,7 +1,7 @@
 @echo off
 cd ..\..\bin
 REM jar cmf META-INF\MANIFEST.MF pcpatreditor.jar .
-jar cf lingtree.jar --main-class org.sil.lingtree.MainApp .
+jar --create --file lingtree.jar --main-class org.sil.lingtree.MainApp .
 copy lingtree.jar ..\installer\windows\input > nul
 del lingtree.jar > nul
 cd ..\installer\windows
